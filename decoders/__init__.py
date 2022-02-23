f21589f993f705d4e037e8d60ae46a871935631f
Yair Mazal <yairmazal@gmail.com>
2022-02-23 16:19:35 +0200

    Add bit flipping decoder based on entropy of data

--- decoders/__init__.py
+++ decoders/__init__.py
@@ -1,4 +1,4 @@
 """package for implementing decoders"""
 from .decoder import Decoder, DecoderType
-from .entropy_bitwise_decoder import EntropyBitwiseDecoder
-__all__: list[str] = ["Decoder", "DecoderType", "EntropyBitwiseDecoder"]
+from .entropy_bitwise_decoder import EntropyBitwiseDecoder, EntropyBitwiseFlippingDecoder
+__all__: list[str] = ["Decoder", "DecoderType", "EntropyBitwiseDecoder", "EntropyBitwiseFlippingDecoder"]
