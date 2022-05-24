7d5e90a7312669b089e3b72f76ae42cfcd5fe650
Yair Mazal <yairmazal@gmail.com>
2022-05-24 17:00:49 +0300

    Add buffers classifier

---
+++ inference/clustering.py
@@ -0,0 +1,114 @@
+from itertools import combinations
+from bitstring import Bits
+from numpy.typing import NDArray
+import numpy as np
+from utils.information_theory import prob, hellinger
+from utils.bit_operations import hamming_distance
+from scipy.stats import entropy
+
+
+class Cluster:
+    def __init__(self, dist: NDArray[np.float_], uid: int, n_samples: int = 1) -> None:
+        self._data: NDArray[np.int_] = np.array(dist * n_samples, dtype=np.int_)
+        self.uid: int = uid
+        self.n_samples = n_samples
+
+    @property
+    def hard_bits(self) -> NDArray[np.int_]:
+        return np.array(self._data[:, 1] > self.dist[:, 0], dtype=np.int_)
+
+    @property
+    def dist(self) -> NDArray[np.float_]:
+        return self._data / self.n_samples
+
+    def add_sample(self, buffer: Bits) -> None:
+        sample = np.array(buffer, dtype=np.int_)
+        self._data[:, 0] += (sample == 0)
+        self._data[:, 1] += (sample == 1)
+        self.n_samples += 1
+
+    def merge_cluster(self, cluster) -> None:
+        self._data += cluster._data
+        self.n_samples += cluster.n_samples
+
+    def __str__(self) -> str:
+        return f'cluster id: {self.uid}, with {self.n_samples} samples'
+
+    def __len__(self) -> int:
+        return self.n_samples
+
+
+class BufferClassifier:
+    def __init__(self, min_data_size: int, k: int, classify_dist: str = "LL", merge_dist: str = "Hellinger",
+                 weight_scheme: str = "exponential") -> None:
+        self.min_data_size: int = min_data_size
+        self.k: int = k
+        self.clusters: list[Cluster] = []
+        self.init: bool = False
+        self.__cluster_uid: int = 0
+        self.samples: int = 0
+        self.classify_dist = classify_dist
+        self.merge_dist = merge_dist
+        self.weight_scheme = weight_scheme
+
+    def classify(self, buffer: Bits) -> int:
+        self.samples += 1
+        if self.samples < self.min_data_size:
+            sample = np.array(buffer, dtype=np.int_)[np.newaxis].T
+            self.__add_cluster(prob(sample))
+            return -1
+        if not self.init:
+            self.merge_clusters()
+            self.init = True
+
+        metric = None
+        for cluster in self.clusters:
+            if self.classify_dist == "hamming":
+                d = hamming_distance(buffer, cluster.hard_bits)
+                if (metric is None) or (d < metric):
+                    metric = d
+                    closest: Cluster = cluster
+            else:  # use log likelihood
+                ll = np.mean(np.clip(
+                    np.log(cluster.dist[:, 1] + np.finfo(np.float_).eps) * np.array(buffer, dtype=np.int_) + np.log(
+                        cluster.dist[:, 0] + np.finfo(np.float_).eps) * (1 - np.array(buffer, dtype=np.int_)),
+                    -100, 0))
+                if (metric is None) or (ll > metric):
+                    metric = ll
+                    closest = cluster
+
+        closest.add_sample(buffer)
+        return closest.uid
+
+    def __add_cluster(self, dist: NDArray[np.float_]) -> Cluster:
+        self.clusters.append(Cluster(dist, self.__cluster_uid))
+        self.__cluster_uid += 1
+        return self.clusters[-1]
+
+    def merge_clusters(self) -> None:
+        while len(self.clusters) > self.k:
+            # find all pairs of clusters
+            indices = list(combinations(range(len(self.clusters)), 2))
+            min_dist: float = None  # type: ignore
+            for c1, c2 in indices:
+                mx = (len(self.clusters[c1]) + len(self.clusters[c2]))/2
+                if self.weight_scheme == "sqrt":
+                    w = np.sqrt(mx)  # square root weights
+                elif self.weight_scheme == "linear":
+                    w = mx
+                else:  # no weighing used
+                    w = 1
+                if self.merge_dist == "KlDiv":
+                    d = w*np.mean(np.clip(entropy(self.clusters[c1].dist, self.clusters[c2].dist, base=2, axis=1) +
+                                          entropy(self.clusters[c2].dist, self.clusters[c1].dist, base=2, axis=1),
+                                          0, 10))
+                else:  # hellinger distance
+                    d = w*np.mean(hellinger(self.clusters[c1].dist, self.clusters[c2].dist))
+                if (min_dist is None) or (d < min_dist):
+                    min_dist = d
+                    pair = c1, c2
+            c: Cluster = self.clusters.pop(pair[1])
+            self.clusters[pair[0]].merge_cluster(c)
+
+
+__all__: list[str] = ["Cluster", "BufferClassifier"]
---
+++ scripts/clustering_trials.py
@@ -0,0 +1,94 @@
+import pickle
+from bitstring import Bits, BitArray
+import numpy as np
+from inference import BufferClassifier
+
+
+with open('../runs/HC_eilat_July_2018/data/hc_to_ship.pickle', 'rb') as f:
+    hc_tx = pickle.load(f)
+rng = np.random.default_rng()
+two_sec_bin = [Bits(auto=tx.get("bin")) for tx in hc_tx.get("20000")]
+weight_scheme = "sqrt"  # see implemented schemes in merge_clusters method of BufferClassifier
+print(weight_scheme)
+
+
+# option 1: use rate 1/2 with N=1296, k=648, and two classes of buffers
+buffers = []
+n = 1296
+r = 1/2
+p = 0.07
+k = int(n*r)
+pad_len = k - len(two_sec_bin[0]) // 2
+no_errors = int(k*p)
+n_classes = 2
+n_training = 100
+for b in two_sec_bin:
+    errors = rng.choice(k, size=no_errors, replace=False)
+    corrupted = BitArray(b[:576] + Bits(auto=rng.integers(low=0, high=2, size=k-576)))
+    for idx in errors:
+        corrupted[idx] = not corrupted[idx]
+    buffers.append((Bits(corrupted), 0))
+
+    errors = rng.choice(k, size=no_errors, replace=False)
+    corrupted = BitArray(b[576:])
+    for idx in errors:
+        corrupted[idx] = not corrupted[idx]
+    buffers.append((Bits(corrupted), 1))
+
+    # buffers.append((b[:612] + Bits(auto=rng.integers(low=0, high=2, size=pad_len)), 0))
+    # buffers.append((b[612:] + Bits(auto=rng.integers(low=0, high=2, size=pad_len)), 1))
+
+    # buffers.append((b[:612], 0))
+    # buffers.append((b[612:], 1))
+# classification metrics can be "hamming" or "LL".
+# merging metrics can be "KlDiv" (symmetric) and "Hellinger". KlDiv underperformed due to unbound nature. linear
+classifier = BufferClassifier(n_training, n_classes, classify_dist="LL", merge_dist="Hellinger", weight_scheme=weight_scheme)
+actual_classes = np.zeros(len(buffers), dtype=np.int_)
+labels = np.zeros(len(buffers), dtype=np.int_)
+for idx, b in enumerate(buffers):
+    actual_classes[idx] = b[1]
+    labels[idx] = classifier.classify(b[0])
+result_1 = [np.unique(labels[n_training - 1 + i :: n_classes], return_counts=True) for i in range(n_classes)]
+
+# option 2: use rate 3/4 with N=648, k=486, and three classes of buffers
+buffers = []
+n = 648
+r = 3/4
+p = 0.07
+k = int(n*r)
+pad_len = k - len(two_sec_bin[0]) // 2
+no_errors = int(k*p)
+n_classes = 3
+n_training = 100
+for b in two_sec_bin:
+    errors = rng.choice(k, size=no_errors, replace=False)
+    corrupted = BitArray(b[:416] + Bits(auto=rng.integers(low=0, high=2, size=k-416)))
+    for idx in errors:
+        corrupted[idx] = not corrupted[idx]
+    buffers.append((Bits(corrupted), 0))
+
+    errors = rng.choice(k, size=no_errors, replace=False)
+    corrupted = BitArray(b[416:864] + Bits(auto=rng.integers(low=0, high=2, size=k-(864-416))))
+    for idx in errors:
+        corrupted[idx] = not corrupted[idx]
+    buffers.append((Bits(corrupted), 1))
+
+    errors = rng.choice(k, size=no_errors, replace=False)
+    corrupted = BitArray(b[864:] + Bits(auto=rng.integers(low=0, high=2, size=k - (1224 - 864))))
+    for idx in errors:
+        corrupted[idx] = not corrupted[idx]
+    buffers.append((Bits(corrupted), 2))
+    # buffers.append((b[:612] + Bits(auto=rng.integers(low=0, high=2, size=pad_len)), 0))
+    # buffers.append((b[612:] + Bits(auto=rng.integers(low=0, high=2, size=pad_len)), 1))
+
+# classification metrics can be "hamming" or "LL".
+# merging metrics can be "KlDiv" (symmetric) and "Hellinger". KlDiv underperformed due to unbound nature. linear
+classifier = BufferClassifier(n_training, n_classes, classify_dist="LL", merge_dist="Hellinger", weight_scheme=weight_scheme)
+actual_classes = np.zeros(len(buffers), dtype=np.int_)
+labels = np.zeros(len(buffers), dtype=np.int_)
+for idx, b in enumerate(buffers):
+    actual_classes[idx] = b[1]
+    labels[idx] = classifier.classify(b[0])
+result_2 = [np.unique(labels[n_training - 1 + i::n_classes], return_counts=True) for i in range(n_classes)]
+# values1, counts1 = np.unique(labels[99::3], return_counts=True)
+# values2, counts2 = np.unique(labels[100::2], return_counts=True)
